9521b1b967c8d54a9cc6ee47fcd2f2cb570bafd2
Seth Anderson <seth@hellonesh.io>
2022-06-29 14:31:44 -0500

    more annoying updates

--- rps.py
+++ rps.py
@@ -1,91 +1,84 @@
+from concurrent.futures import thread
 import random
+from rps_helper import (
+    tie_game,
+    you_win,
+    steve_austin_wins,
+    the_rock_wins,
+    ask_to_play_winner,
+    show_player_results,
+)
 
-#testicles = [1, 3, 5, 7, 45]
+# testicles = [1, 3, 5, 7, 45]
 
-#print( "the complete list of testes is: " + str(testicles))
+# print( "the complete list of testes is: " + str(testicles))
 
-#random_teste = random.choice(testicles)
+# random_teste = random.choice(testicles)
 
-#print ("The randomly selected testicle is : " + str(random_teste))
-
-TIE = "tie!!\n"
-
-ROCK = "Rock"
-PAPER = "Paper"
-SCISSORS = "Scissors"
-
-STEVE_WINS = "STONE COLD WINS!!!\n"
-ROCK_WINS = "THE ROCK WINS!!!\n"
+# print ("The randomly selected testicle is : " + str(random_teste))
 
 STEVE_NAME = "Stone Cold Steve Austin"
 ROCK_NAME = 'Dwayne "The Rock" Johnson'
 
-PROMPT_USER = 'Rock, paper, or scissors baby? '
-YOU_WIN = "YOU WIN!!!\n"
-
+# Game states
+ROCK = "Rock"
+PAPER = "Paper"
+SCISSORS = "Scissors"
 RPS = [ROCK, PAPER, SCISSORS]
 
 winner = 0
 
 while winner == 0:
-    theRock = random.choice(RPS)
-    stoneCold = random.choice(RPS)
+    the_rock = random.choice(RPS)
+    stone_cold = random.choice(RPS)
+    show_player_results(the_rock, ROCK_NAME, stone_cold, STEVE_NAME)
 
-
-    print (f"{ROCK_NAME} picked: {str(theRock)}!")
-    print (f"{STEVE_NAME} picked: {str(stoneCold)}!")
-
-    if theRock == stoneCold:
-            print (TIE)
-    elif theRock == PAPER:
-        if stoneCold == SCISSORS:
-                print (STEVE_WINS)
-                winner = STEVE_NAME
+    if the_rock == stone_cold:
+        tie_game()
+    elif the_rock == PAPER:
+        if stone_cold == SCISSORS:
+            steve_austin_wins()
+            winner = STEVE_NAME
         else:
-                print (ROCK_WINS)
-                winner = ROCK_NAME
-    elif theRock == SCISSORS:
-        if stoneCold == ROCK:
-                print (STEVE_WINS)
-                winner = STEVE_NAME
+            the_rock_wins()
+            winner = ROCK_NAME
+    elif the_rock == SCISSORS:
+        if stone_cold == ROCK:
+            steve_austin_wins()
+            winner = STEVE_NAME
         else:
-                print (ROCK_WINS)
-                winner = ROCK_NAME
-    elif theRock == ROCK:
-        if stoneCold == PAPER:
-                print (STEVE_WINS)
-                winner = STEVE_NAME
+            the_rock_wins()
+            winner = ROCK_NAME
+    elif the_rock == ROCK:
+        if stone_cold == PAPER:
+            steve_austin_wins()
+            winner = STEVE_NAME
         else:
-                print (ROCK_WINS)
-                winner = ROCK_NAME
+            the_rock_wins()
+            winner = ROCK_NAME
 
-#SECOND MATCH! THIS ONE IS FOR THE BELT
-print (f"Want to square off with {str(winner)}?")
-face = input(PROMPT_USER)
+# SECOND MATCH! THIS ONE IS FOR THE BELT
+
+face = ask_to_play_winner(winner)
 heel = random.choice(RPS)
-
-print (f"You picked: {str(face)}!")
-print (f"{str(winner)} picks: {str(heel)}!!!")
+show_player_results(heel, winner, face)
 
 if heel.casefold() == face.casefold():
-        print (TIE)
+    tie_game()
 elif heel.casefold() == PAPER:
     if face.casefold() == SCISSORS:
-            print (YOU_WIN)
-            
+        you_win()
     else:
-            print (f"{str(winner)} WINS!!!")
-            
+        print(f"{str(winner)} WINS!!!")
+
 elif heel.casefold() == SCISSORS:
     if face.casefold() == ROCK:
-            print (YOU_WIN)
-            
+        you_win()
     else:
-            print (f"{str(winner)} WINS!!!")
-            
+        print(f"{str(winner)} WINS!!!")
+
 elif heel.casefold() == ROCK:
     if face.casefold() == PAPER:
-            print (YOU_WIN)
-            
+        you_win()
     else:
-            print (f"{str(winner)} WINS!!!")
+        print(f"{str(winner)} WINS!!!")
